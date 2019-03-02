325b4ee8e7fb9204def400af9df89fa5a18a20ee
Alex Tavares <alex.tavares@yahoo.com>
2019-03-02 23:11:32 +0000

    sketching layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
+import { Layout, Row, Col } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    
+  }
+
   render() {
+    const {
+        Header, Footer, Content
+      } = Layout;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+        <Layout>
+          <Header>Alex's Challenge</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+    
     );
   }
 }
