a0df713d5c470f17f0bdd9ec1b6487bb5d2ea0cc
Alex Tavares <alex.tavares@yahoo.com>
2019-03-13 03:01:18 +0000

    Updates

---
+++ precache-manifest.52a80f7b63591bc1cdc18cbdb18b5d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e3e5999f121c0b1a5b8",
+    "url": "/challenge-chart-plot/static/js/runtime~main.2e3e5999.js"
+  },
+  {
+    "revision": "ca5b129b9d4a3445e468",
+    "url": "/challenge-chart-plot/static/js/main.ca5b129b.chunk.js"
+  },
+  {
+    "revision": "6781aaac192a0ec5e06f",
+    "url": "/challenge-chart-plot/static/js/2.6781aaac.chunk.js"
+  },
+  {
+    "revision": "ca5b129b9d4a3445e468",
+    "url": "/challenge-chart-plot/static/css/main.67d6a5cb.chunk.css"
+  },
+  {
+    "revision": "6781aaac192a0ec5e06f",
+    "url": "/challenge-chart-plot/static/css/2.c7cb8a47.chunk.css"
+  },
+  {
+    "revision": "83c9ff366ed9f82b63739309e5c2142c",
+    "url": "/challenge-chart-plot/index.html"
+  }
+];
